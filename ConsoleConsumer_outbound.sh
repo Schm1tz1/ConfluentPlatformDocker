cc79e89b61aee3f01ab696594c665e37884d79a5
Schm1tz1 <ro-man@web.de>
2019-07-17 13:46:05 +0200

    Removed old bash scripts

--- ConsoleConsumer_outbound.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-cd cp-docker-images/examples/cp-all-in-one
-docker exec -it broker kafka-console-consumer --bootstrap-server localhost:29092 --topic outputData --property print.key=true --property print.timestamp=true
